3efd57b8ea78362342a3302609aaf1a2cdfcf033
sureshselvadurai <sureshrajaselvadurai@gmail.com>
2024-03-01 19:11:32 -0800

    EC4

--- src/main/java/solutions/EC4.java
+++ src/main/java/solutions/EC4.java
@@ -4,10 +4,12 @@ import java.util.Arrays;
 //Problem 1 : 1329. Sort the Matrix Diagonally (Medium)
 //A matrix diagonal is a diagonal line of cells starting from some cell in either the topmost row or leftmost column and going in the bottom-right direction until reaching the matrix's end. For example, the matrix diagonal starting from mat[2][0], where mat is a 6 x 3 matrix, includes cells mat[2][0], mat[3][1], and mat[4][2].
 //Given an m x n matrix mat of integers, sort each matrix diagonal in ascending order and return the resulting matrix.
+//Reference  :https://leetcode.com/problems/sort-the-matrix-diagonally/
 
 //Problem 2 : 451. Sort Characters By Frequency (Medium)
 //        Given a string s, sort it in decreasing order based on the frequency of the characters. The frequency of a character is the number of times it appears in the string.
 //        Return the sorted string. If there are multiple answers, return any of them.
+//Reference : https://leetcode.com/problems/sort-characters-by-frequency/description/
 
 public class EC4 {
     public static int[][] diagonalSort(int[][] mat) {
